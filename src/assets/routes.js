eb7aee83ea319efb2b741e6b231a4b4a9b58d95e
wangzimei <wzm@nebulas.io>
2018-03-09 20:27:59 +0800

    +

--- src/assets/routes.js
+++ src/assets/routes.js
@@ -1,14 +1,14 @@
 
 module.exports = [{
-    component: require("@/routes/vue-wallet").default,
-    path: "/wallet"
-}, {
     redirect: "wallet",
     path: "/"
+}, {
+    component: require("@/routes/vue-contracts").default,
+    path: "/contracts"
 }, {
     component: require("@/routes/vue-transation").default,
     path: "/transation"
 }, {
-    component: require("@/routes/vue-contracts").default,
-    path: "/contracts"
+    component: require("@/routes/vue-wallet").default,
+    path: "/wallet"
 }];
