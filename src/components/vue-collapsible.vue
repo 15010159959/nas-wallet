0bf456caebf60bcd3be2693a350649cc00cf9182
wangzimei <wzm@nebulas.io>
2018-03-09 22:18:16 +0800

    +

---
+++ src/components/vue-collapsible.vue
@@ -0,0 +1,69 @@
+<style>
+    .vue-collapsible {
+        background-color: beige;
+        border-radius: 3px;
+        margin: 12px;
+        overflow: hidden;
+    }
+
+    .vue-collapsible .hand-bar {
+        background-color: rgb(227, 229, 151);
+        color: white;
+        line-height: 19px;
+        text-align: center;
+        transition: background-color .5s ease;
+    }
+
+    .vue-collapsible.collapse .hand-bar {
+        background-color: rgb(115, 189, 115);
+    }
+
+    .vue-collapsible .hand-bar::before {
+        content: "⇧";
+        display: inline-block;
+        transition: transform .3s ease;
+    }
+
+    .vue-collapsible.collapse .hand-bar::before {
+        transform: rotate(180deg);
+    }
+
+    .vue-collapsible .line1 {
+        line-height: 30px;
+    }
+
+    .vue-collapsible .line2 {
+        height: 140px;
+        overflow: hidden;
+        transition: height .5s ease;
+    }
+
+    .vue-collapsible.collapse .line2 {
+        height: 0;
+    }
+</style>
+<template>
+    <div class=vue-collapsible v-bind:class={collapse}>
+        <div class=hand-bar v-on:click="collapse = !collapse"></div>
+        <div class=line1>left icon, right address</div>
+        <div class=line2>
+            <br> 1
+            <br> 2
+            <br> 3
+            <br> 4
+            <br> 5
+            <br> 6
+            <br> 7
+        </div>
+    </div>
+</template>
+<script>
+    module.exports = {
+        data() {
+            return {
+                collapse: true
+
+            };
+        }
+    };
+</script>
--- src/routes/vue-wallet.vue
+++ src/routes/vue-wallet.vue
@@ -1,18 +1,28 @@
 <style>
     .vue-wallet {
-            background-color: gray;
-            height: 600px;
-            margin-top: 8px;
-        }
+        background-color: gray;
+        height: 600px;
+        margin-top: 8px;
+        overflow: auto;
+    }
+
+    .vue-wallet .vue-collapsible {
+        width: 320px;
+    }
 </style>
 <template>
     <div class=vue-wallet>
-        <h1>wallet</h1>
+        <vue-collapsible></vue-collapsible>
+
+        <div class=add>Add Wallet</div>
     </div>
 </template>
 
 <script>
     module.exports = {
+        components: {
+            "vue-collapsible": require("@/components/vue-collapsible").default
+        },
         data() {
             return {
                 msg: 'Welcome to Your Vue.js App'
